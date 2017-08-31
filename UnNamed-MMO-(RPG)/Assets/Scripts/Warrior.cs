99765aee9999ebfd6c9ab01075dafbf39599f344
FireHogGames <hidde01@live.nl>
2017-08-31 17:54:16 +0200

    Gestart met het project

---
+++ UnNamed-MMO-(RPG)/Assets/Scripts/Warrior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Warrior : MonoBehaviour {
+
+    private NavMeshAgent agent;
+
+    public Transform target;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        MoveWarrior();
+    }
+
+    private void MoveWarrior()
+    {
+        Vector3 targetPos = new Vector3(target.position.x, target.position.y, target.position.z);
+
+        agent.SetDestination(targetPos);
+    }
+}
